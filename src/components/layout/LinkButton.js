c0c759ce7ca6cebbeb23884a0b945211f0f13e1c
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-02-26 22:10:04 -0300

    Aula 20: Criando a home

---
+++ src/components/layout/LinkButton.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./LinkButton.module.css";
+
+export default function LinkButton({ to, text }) {
+  return (
+    <Link className={ styles.btn } to={ to }>
+      { text }
+    </Link>
+  );
+}
+
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+import styles from "./Home.module.css";
+import savings from "../../img/savings.svg";
+import LinkButton from "../layout/LinkButton";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className={ styles.home_container }>
+      <h1>Welcome to <span>Costs</span></h1>
+      <p>Start managing your projects right now!</p>
+      <LinkButton to="/newproject" text="Create Project" />
+      <img src={ savings } alt="Costs" />
+    </section>
+  );
 }

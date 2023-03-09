ea10b16c5d37db9daef0fc4760a8c786f3be32ef
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-03-08 22:09:37 -0300

    Aula 23: Conectando com API pelo React

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -3,12 +3,15 @@ import React from "react";
 
 import styles from "./Select.module.css";
 
-export default function Select({ text, name }) {
+export default function Select({ text, name, options }) {
   return (
     <div className={ styles.form_control }>
       <label htmlFor={ name }>{ text }</label>
       <select name={ name } id={ name }>
         <option>Select an option</option>
+        { options.map((option) => (
+          <option value={ option.id } key={ option.id }>{ option.name }</option>
+        )) }
       </select>
     </div>
   );
--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./ProjectForm.module.css";
 import Input from "../form/Input";
@@ -7,12 +7,27 @@ import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 export default function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => setCategories(data))
+      .catch(error => console.log(error));
+
+  }, []);
+
   return (
-    <form className={ styles.form }>
+    <form className={styles.form}>
       <Input type="text" text="Project name:" name="name" placeholder="Enter project name" />
       <Input type="number" text="Total budget:" name="buget" placeholder="Enter total budget" />
-      <Select name="category_id" text="Select category:" />
-      <SubmitButton text={ btnText }/>
+      <Select name="category_id" text="Select category:" options={categories} />
+      <SubmitButton text={btnText} />
     </form>
   );
 }
